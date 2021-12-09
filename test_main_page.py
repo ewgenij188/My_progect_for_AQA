fd173401518d3cc054c1ac38e2317014e4d2dcc5
Жук Евгений Викторович <ezhuk@MAPSOFT.BY>
2021-12-09 09:37:57 +0300

    add swick-over for new page

--- test_main_page.py
+++ test_main_page.py
@@ -27,3 +27,10 @@ def test_should_be_register_form(browser):
     page.open()
     page.should_be_register_form()
 
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
